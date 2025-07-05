de61ce208d40cf3f0e939143bd8d87ed7f4ddf34
Ankur Ashish <ankurashishindia@gmail.com>
2025-07-05 17:42:18 +0530

    update index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,24 @@ module.exports = {
   },
   plugins: [],
 };
+
+// tailwind.config.js
+module.exports = {
+  content: ["./*.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".no-scrollbar": {
+          scrollbarWidth: "none", // Firefox
+          "-ms-overflow-style": "none", // IE 10+
+        },
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none", // Chrome, Safari, Opera
+        },
+      });
+    },
+  ],
+};
